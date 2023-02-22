fd421f8625d67b8db5d84b79d065b02aeb2693ce
musfiqur521 <musfiqur521@gmail.com>
2023-02-23 00:19:21 +0600

    deposit and balance

--- js/deposit.js
+++ js/deposit.js
@@ -2,10 +2,33 @@ document.getElementById('btn-deposit').addEventListener('click',function(){
     //get the deposit amount form the deposit input field
     // for input field use .value to the value inside the input field
     const depositField = document.getElementById('deposit-field');
-    const depositAmount = depositField.value;
+    const newDepositAmountString = depositField.value;
+    const newDepositAmount = parseFloat(newDepositAmountString);
 
     //deposit the current amount total
     // for non-input use innerText to get the text
     const depositTotalElement = document.getElementById('deposit-total');
-    const depositTotal = depositTotalElement.value;
+    const previousDepositTotalString = depositTotalElement.innerText;
+    const previousDepositTotal = parseFloat(previousDepositTotalString);
+
+    // add numbers to set the total deposit 
+    const currentDepositTotal = previousDepositTotal + newDepositAmount;
+
+    depositTotalElement.innerText = currentDepositTotal;
+
+    // get balance current total
+
+    const balanceTotalElement = document.getElementById('balance-total');
+    const previousBalanceTotalString = balanceTotalElement.innerText;
+    const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    // calculate current total balance
+    const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
+
+    //set the balance total
+    balanceTotalElement.innerText = currentBalanceTotal;
+
+
+    // clear the deposit field
+    depositField.value = '';
 })
